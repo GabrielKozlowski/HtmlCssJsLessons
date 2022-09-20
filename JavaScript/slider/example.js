e4f973cf4d84a1f8ee18bcb090586636311c7f7c
Gabrielkozlowski <gabriel89krk@gmail.com>
2022-09-20 19:20:41 +0200

    more examples

--- JavaScript/slider/example.js
+++ JavaScript/slider/example.js
@@ -1,45 +1,11 @@
-let myFirstFunction = (name,greeting) => {
-    console.log(greeting + ' mam na imię ' + name + ' !');
+let isBiggerThab10 = (number) => {
+    if(number > 10){
+        console.log('Liczba jest większa od 10');
+    }
+    else {
+        console.log('Liczba nie jest większa od 10');
+    }
 };
 
-myFirstFunction('Gabriel', 'Hey');
-myFirstFunction('Jan', 'Witam')
-
-let multiplyByTwo = (num) => {
-    console.log(num * 2);
-};
-
-let multiplyByNumber = (num1, num2) => {
-    console.log(num1 * num2);
-};
-
-let powerToTwo = (num) => {
-    console.log(num ** 2);
-};
-
-multiplyByTwo(3)
-multiplyByNumber(2,4)
-powerToTwo(4)
-
-
-let car1 = {
-    'mark': 'Fiat',
-    'model': 'Punto',
-    'productionYear': 'Rok produkcji: 2021',
-};
-
-let car2 = {
-    'mark': 'BMW',
-    'model': 'E36',
-    'productionYear': 'Rok produkcji: 2018',
-};
-
-console.log(car1.mark, car1.model, car1.productionYear);
-console.log(car2.mark, car2.model, car2.productionYear);
-
-
-let C = 299792458;
-console.log(C);
-let M = 10;
-let E = M*(C*C)
-console.log(E);
+isBiggerThab10(11);
+isBiggerThab10(3);
