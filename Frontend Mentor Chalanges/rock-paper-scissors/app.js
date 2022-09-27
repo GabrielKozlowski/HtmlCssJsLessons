97d92de7cf60fb29707ab4ec63802026512b727b
Gabrielkozlowski <gabriel89krk@gmail.com>
2022-09-27 14:37:58 +0200

    allmost finished

--- Frontend Mentor Chalanges/rock-paper-scissors/app.js
+++ Frontend Mentor Chalanges/rock-paper-scissors/app.js
@@ -6,9 +6,15 @@
 const playerWinsLSKey = "playerWins";
 const AiWinsLSKey = "AiWins";
 
+const winningResultsMap = {
+    paper: ['rock'],
+    rock: ['scissors'],
+    scissors: ['paper'],
+}
+
 let state = {
-    playerWins: localStorage.getItem(playerWinsLSKey) || 0,
-    AiWins: localStorage.getItem(AiWinsLSKey) || 0,
+    playerWins: Number(localStorage.getItem(playerWinsLSKey)) || 0,
+    AiWins: Number(localStorage.getItem(AiWinsLSKey)) || 0,
     playerPick: null,
     AiPick: null,
 };
@@ -22,6 +28,7 @@ const bindPickEvents = () => {
     document.querySelectorAll(".options button").forEach((button) => {
         button.addEventListener("click", pick);
     });
+    document.querySelector(".result__button").addEventListener("click", reset);
 };
 
 const pick = (e) => {
@@ -38,7 +45,6 @@ const pickByPlayer = (pickedOption) => {
     };
 };
 
-
 const pickByAi = () => {
     const options = ["rock", "paper", "scissors"];
     const AiPick = options[Math.floor(Math.random() * options.length)];
@@ -50,37 +56,105 @@ const pickByAi = () => {
 };
 
 const hideOptions = () => {
-    document.querySelector(".options").classList.add("hidden");
+    const optionsElement = document.querySelector(".options");
+    optionsElement.classList.add("slide-left");
 };
 
 const showFight = () => {
-    document.querySelector(".fight").classList.remove("hidden");
+    const fightElement = document.querySelector(".fight");
+    fightElement.classList.add("slide-left");
     createElementPickedByPlayer();
-    // createElementPickedByAi();
+    createElementPickedByAi();
+
+    document.querySelectorAll(".options button").forEach((button) => {
+        button.setAttribute("tabindex", -1);
+    });
+
+    document.querySelector(".result__button").setAttribute("tabindex", 0);
+
+    showResult()
+};
+
+const showResult = () => {
+    const resultTextElement = document.querySelector(".result__text");
+    if (state.playerPick === state.AiPick) {
+        resultTextElement.innerText = "DRAW"
+    } else if (winningResultsMap[state.playerPick].includes(state.AiPick)) {
+        resultTextElement.innerText = "YOU WIN"
+        localStorage.setItem(playerWinsLSKey, state.playerWins + 1);
+        state = {
+            ...state,
+            playerWins: state.playerWins + 1,
+        };
+    } else {
+        resultTextElement.innerText = "YOU LOSE"
+        localStorage.setItem(playerWinsLSKey, state.AiWins + 1);
+        state = {
+            ...state,
+            AiWins: state.AiWins + 1.
+        };
+    };
+
+    setTimeout(renderResult, 1000);
+
+    renderScore();
+};
+
+const renderResult = () => {
+    document.querySelector(".result").classList.add("shown");
+    document.querySelector(".pick--player").classList.add("moved");
+    document.querySelector(".pick--ai").classList.add("moved");
 };
 
 const createElementPickedByPlayer = () => {
     const playerPick = state.playerPick;
 
-    const buttonElement = document.createElement("div");
-    buttonElement.classList.add('button', 'button--${playerPick}');
+    const pickContainerElement = document.querySelector(".pick__container--player");
+    
+    pickContainerElement.innerHTML = "";
+    pickContainerElement.appendChild(createPickElement(playerPick));
+};
+
+const createElementPickedByAi = () => {
+    const AiPick = state.AiPick;
+
+    const pickContainerElement = document.querySelector(".pick__container--ai");
+    
+    pickContainerElement.innerHTML = "";
+    pickContainerElement.appendChild(createPickElement(AiPick));
+}
+
+const createPickElement = (option) => {
+    const pickElement = document.createElement("div");
+    pickElement.classList.add('button', `button--${option}`);
 
     const imageContainerELement = document.createElement("div");
     imageContainerELement.classList.add("button__image-container");
 
     const imgElement = document.createElement("img");
-    imgElement.src = './images/icon-${playerPick}.svg';
-    imgElement.alt = playerPick; 
+    imgElement.src = `./images/icon-${option}.svg`;
+    imgElement.alt = option; 
 
     imageContainerELement.appendChild(imgElement);
 
-    buttonElement.appendChild(imageContainerELement);
+    pickElement.appendChild(imageContainerELement);
+    
+    return pickElement;
+}
 
-    const pickContainerElement = document.querySelector(".pick__container--player");
-    pickContainerElement.innerHTML = "";
-    pickContainerElement.appendChild(buttonElement);
-};
+const reset = () => {
+    const fightElement = document.querySelector(".fight");
+    fightElement.classList.remove("slide-left");
 
+    const optionsElement = document.querySelector(".options");
+    optionsElement.classList.remove("slide-left");
+
+    document.querySelectorAll(".options button").forEach((button) => {
+        button.setAttribute("tabindex", 0);
+    });
+
+    document.querySelector(".result__button").setAttribute("tabindex", 1);
+}
 
 const init = () => {
     renderScore();
