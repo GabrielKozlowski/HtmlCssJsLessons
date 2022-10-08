d814e49c6fb0ef81cc0a00621add780264b5a3d8
Gabrielkozlowski <gabriel89krk@gmail.com>
2022-10-08 19:48:29 +0200

    task completed

--- JS CSS Chalange/junior/notifications-page-main/app.js
+++ JS CSS Chalange/junior/notifications-page-main/app.js
@@ -1,23 +1,23 @@
 const unReadMessages = document.querySelectorAll(".unread");
-const unread = document.getElementById(".notifications");
-const markAll = document.getElementById(".mark__all");
+const unread = document.getElementById("notifications");
+const markAll = document.getElementById("mark__all");
 
 
 unread.innerText = unReadMessages.length;
 
-unReadMessages.forEach((comment) => {
-    comment.addEventListener("click", () => {
-        comment.classList.remove("unread");
-        const newUnreadMessages = document.querySelectorAll("unread");
+unReadMessages.forEach((message) => {
+    message.addEventListener("click", () => {
+        message.classList.remove("unread");
+        const newUnreadMessages = document.querySelectorAll(".unread");
         unread.innerText = newUnreadMessages.length;
     })
 })
 
 
 markAll.addEventListener("click", () => {
-    unReadMessages.forEach((comment) => {
-        comment.classList.remove("unread")
+    unReadMessages.forEach((message) => {
+        message.classList.remove("unread")
     })
-    const newUnreadMessages = document.querySelectorAll("unread");
+    const newUnreadMessages = document.querySelectorAll(".unread");
     unread.innerText = newUnreadMessages.length;
 })
