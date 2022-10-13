14fcb285fbf22a05137079242e1f3338816d6711
Gabrielkozlowski <gabriel89krk@gmail.com>
2022-10-13 19:43:45 +0200

    next task

--- JS CSS Chalange/junior/expenses-chart-component-main/app.js
+++ JS CSS Chalange/junior/expenses-chart-component-main/app.js
@@ -1,10 +1,10 @@
-const charMon = document.getElementById("char1");
-const charTue = document.getElementById("char2");
-const charWed = document.getElementById("char3");
-const charThu = document.getElementById("char4");
-const charFri = document.getElementById("char5");
-const charSat = document.getElementById("char6");
-const charSun = document.getElementById("char7");
+const chartMon = document.getElementById("chart1");
+const chartTue = document.getElementById("chart2");
+const chartWed = document.getElementById("chart3");
+const chartThu = document.getElementById("chart4");
+const chartFri = document.getElementById("chart5");
+const chartSat = document.getElementById("chart6");
+const chartSun = document.getElementById("chart7");
 
 const valueMon = document.getElementById("value1");
 const valueTue = document.getElementById("value2");
@@ -25,58 +25,58 @@ valueSun.style.visibility = "hidden";
 
 
 
-charMon.addEventListener("mouseover", function handleMouseOver() {
+chartMon.addEventListener("mouseover", function handleMouseOver() {
     valueMon.style.visibility = "visible";
 });
 
-charMon.addEventListener("mouseout", function handleMouseOut() {
+chartMon.addEventListener("mouseout", function handleMouseOut() {
     valueMon.style.visibility = "hidden";
 });
 
-charTue.addEventListener("mouseover", function handleMouseOver() {
+chartTue.addEventListener("mouseover", function handleMouseOver() {
     valueTue.style.visibility = "visible";
 });
 
-charTue.addEventListener("mouseout", function handleMouseOut() {
+chartTue.addEventListener("mouseout", function handleMouseOut() {
     valueTue.style.visibility = "hidden";
 });
 
-charWed.addEventListener("mouseover", function handleMouseOver() {
+chartWed.addEventListener("mouseover", function handleMouseOver() {
     valueWed.style.visibility = "visible";
 });
 
-charWed.addEventListener("mouseout", function handleMouseOut() {
+chartWed.addEventListener("mouseout", function handleMouseOut() {
     valueWed.style.visibility = "hidden";
 });
 
-charThu.addEventListener("mouseover", function handleMouseOver() {
+chartThu.addEventListener("mouseover", function handleMouseOver() {
     valueThu.style.visibility = "visible";
 });
 
-charThu.addEventListener("mouseout", function handleMouseOut() {
+chartThu.addEventListener("mouseout", function handleMouseOut() {
     valueThu.style.visibility = "hidden";
 });
 
-charFri.addEventListener("mouseover", function handleMouseOver() {
+chartFri.addEventListener("mouseover", function handleMouseOver() {
     valueFri.style.visibility = "visible";
 });
 
-charFri.addEventListener("mouseout", function handleMouseOut() {
+chartFri.addEventListener("mouseout", function handleMouseOut() {
     valueFri.style.visibility = "hidden";
 });
 
-charSat.addEventListener("mouseover", function handleMouseOver() {
+chartSat.addEventListener("mouseover", function handleMouseOver() {
     valueSat.style.visibility = "visible";
 });
 
-charSat.addEventListener("mouseout", function handleMouseOut() {
+chartSat.addEventListener("mouseout", function handleMouseOut() {
     valueSat.style.visibility = "hidden";
 });
 
-charSun.addEventListener("mouseover", function handleMouseOver() {
+chartSun.addEventListener("mouseover", function handleMouseOver() {
     valueSun.style.visibility = "visible";
 });
 
-charSun.addEventListener("mouseout", function handleMouseOut() {
+chartSun.addEventListener("mouseout", function handleMouseOut() {
     valueSun.style.visibility = "hidden";
 });
