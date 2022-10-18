df9cd6592a60c3f741b6332241936611048ba78b
Gabrielkozlowski <gabriel89krk@gmail.com>
2022-10-18 20:47:30 +0200

    continue js

---
+++ JS CSS Chalange/junior/interactive-card-details-form-main/app.js
@@ -0,0 +1,13 @@
+let names = document.getElementById("name");
+let number = document.getElementById("number");
+let month = document.getElementById("month");
+let year = document.getElementById("year");
+let cvc = document.getElementById("cvc");
+
+let numberCard = document.getElementById("numberCard");
+let nameCard = document.getElementById("nameCard");
+let dateCard = document.getElementById("dateCard");
+
+let button = document.getElementById("button");
+
+
